c7273dee6c9c630a82037555fb93a810f9de3de2
Lukas Berger <mail@lukasberger.at>
2017-06-16 15:07:11 +0200

    linux: patched to 3.10.106
    Change-Id: I002a1fd40440151d6b86be666a49b30c1328dc44

--- net/sctp/socket.c
+++ net/sctp/socket.c
@@ -6730,7 +6730,6 @@ static int sctp_wait_for_sndbuf(struct sctp_association *asoc, long *timeo_p,
 		 */
 		sctp_release_sock(sk);
 		current_timeo = schedule_timeout(current_timeo);
-		BUG_ON(sk != asoc->base.sk);
 		sctp_lock_sock(sk);
 
 		*timeo_p = current_timeo;
