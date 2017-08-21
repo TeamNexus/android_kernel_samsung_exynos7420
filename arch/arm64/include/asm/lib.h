665101bcff34e409b6113c7d5454370714e42507
Lukas0610 <mail@lukasberger.at>
2017-08-21 02:25:39 +0200

    arm64: lib: added ASM-based int_sqrt
    Change-Id: I143d831d3cd2057e9dd22febc2e3c8561fa1d6ed

---
+++ arch/arm64/include/asm/lib.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2017 Lukas Berger
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __ASM_LIB_H
+#define __ASM_LIB_H
+
+#define __HAVE_ARCH_INT_SQRT
+extern unsigned long int_sqrt(unsigned long x);
+
+#endif // __ASM_LIB_H
