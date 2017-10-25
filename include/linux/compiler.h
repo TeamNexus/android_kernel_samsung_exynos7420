4c68a60cf1fab2231321aec126f678cc5231f01f
Lukas Berger <mail@lukasberger.at>
2017-10-25 22:54:20 +0200

    include: compiler: added missing definition of __latent_entropy

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -279,6 +279,10 @@ void ftrace_likely_update(struct ftrace_branch_data *f, int val, int expect);
 # define __attribute_const__	/* unimplemented */
 #endif
 
+#ifndef __latent_entropy
+# define __latent_entropy
+#endif
+
 /*
  * Tell gcc if a function is cold. The compiler will assume any path
  * directly leading to the call is unlikely.
